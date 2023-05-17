66110f1b879e7d53f36de34b3f07ee4a84ac394c
Lucas H <lucashcardoso26@gmail.com>
2023-05-17 18:59:14 -0300

    users routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
+const routes = require("./routes");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Google");
-});
+app.use(express.json());
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
